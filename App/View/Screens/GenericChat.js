06ede414b267c517d1b982aa9680424434cb9c74
Kobi Amsellem <kobix.kobix@gmail.com>
2021-04-28 14:33:50 +0300

    firestore read example

--- App/View/Screens/GenericChat.js
+++ App/View/Screens/GenericChat.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+
+
 
 GenericChat = ({ navigation, route }) => {
+    useState
     const feed_type = route.name
+
+
+
+    useEffect(() => {
+        // 
+        firestore().collection('chats').doc('example_chat_id')
+            .onSnapshot(doc => {
+                if (!doc)
+                    return;
+                console.log(doc.data())
+            })
+    }, [])
+
     return (
         <View style={styles.main}>
             <Text style={styles.headline}>
