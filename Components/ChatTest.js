ca637354a5f1fed82d68e2ffbf029eaabb17be0d
Oded-bs <odedbs11@gmail.com>
2021-04-28 14:14:13 +0300

    added class to chatTest

---
+++ Components/ChatTest.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+import { TextInput, Button } from "react-native"
+import { View } from "react-native"
+
+class ChatTest extends Component {
+
+    render() {
+        return(
+            <View></View>
+        )
+    }
+}
+
+export default ChatTest;
