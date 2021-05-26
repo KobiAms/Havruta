b60c018feb104ff0619b4a49927d5f4a22ad68f4
Oded-bs <odedbs11@gmail.com>
2021-05-26 14:52:14 +0300

    fix running bug

--- App/View/Screens/GenericChat.js
+++ App/View/Screens/GenericChat.js
@@ -8,7 +8,7 @@ import { resolvePreset } from '@babel/core';
 
 
 
-let msgToLoad = 11
+let msgToLoad = 15
 let msgToStart = 0
 let endReached=false;
 let flag = false
@@ -60,20 +60,19 @@ GenericChat = ({ navigation, route }) => {
     const onRefresh = React.useCallback(() => {
         console.log("im here")
     setRefreshing(true);
-    then(() => setRefreshing(false));
+    wait(1000).then(() => setRefreshing(false));
      }, []);
 
     sendMessage = () => {
 
         let tempMessage = {user_id: user.email,user_nick: user.name, message: newMessage, date: new Date()}
+        setNewMessage("")
         if (!tempMessage.message.replace(/\s/g, '').length || !auth().currentUser) {
             setNewMessage("")
         }
         else {
             firestore().collection('chats').doc('reporters').update({
                 messages: firestore.FieldValue.arrayUnion(tempMessage)
-            }).then(() => {
-                setNewMessage("")
             }).catch(error => {
                 console.log(error.toString())
             })
@@ -87,12 +86,8 @@ GenericChat = ({ navigation, route }) => {
       }
 
     function loadMore(chat_data) {
-        if(refreshing === true)
-        {
-            return
-        }
         set_loading_more(true)
-        msgToLoad=msgToLoad+5
+        msgToLoad=msgToLoad+7
         firestore().collection('chats').doc('reporters')
         .onSnapshot(doc => {
             if (!doc)
@@ -102,6 +97,7 @@ GenericChat = ({ navigation, route }) => {
             let reversed = doc.data().messages.reverse()
             if(reversed.length < msgToLoad)
             {
+                endReached=true
                 set_chat_data(reversed.slice(msgToStart,reversed.length))
             }else{
                 set_chat_data(reversed.slice(msgToStart,msgToLoad))
@@ -146,7 +142,7 @@ GenericChat = ({ navigation, route }) => {
                     </Text>
                 </View>
                 
-                <FlatList style={styles.list} inverted data={chat_data}  onEndReachedThreshold={0.5} onEndReached={()=> loadMore(chat_data)}  ref={ref => flat_list_ref = ref} keyExtractor={(item, index) => index}
+                <FlatList style={styles.list} inverted data={chat_data}  onEndReachedThreshold={0.2} onEndReached={()=> loadMore(chat_data)}  ref={ref => flat_list_ref = ref} keyExtractor={(item, index) => index}
                     ListFooterComponent={()=>!endReached && <ListFooterComponent/>} renderItem={({ item }) => <ChatMessage item={item} 
                     refreshControl={
                         <RefreshControl
