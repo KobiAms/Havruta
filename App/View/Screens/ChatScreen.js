c146082913256129ab1703d0ba41ad85e10c5572
BenSagir <bensagir.sbr@gmail.com>
2021-06-07 14:50:36 +0300

    delete chat fucntion is added

--- App/View/Screens/ChatScreen.js
+++ App/View/Screens/ChatScreen.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, Dimensions, StyleSheet } from 'react-native';
+import { View, Pressable, FlatList, Dimensions, StyleSheet, Alert } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import Chat from '../Components/ChatItem';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import IconIo from 'react-native-vector-icons/Ionicons';
 
+
 function ChatScreen({ navigation, route }) {
     const [chats, setChats] = useState([])
     const [isAdmin, setIsAdmin] = useState(false);
 
+    /*this useEffect update the state "isAdmin" after checking it's role on firebase*/
     useEffect(() => {
         const subscriber = firestore()
             .collection('users')
@@ -22,6 +24,9 @@ function ChatScreen({ navigation, route }) {
         return subscriber;
     }, []);
 
+    /*this useEffect update the state array "chatName", and put an array of objects.
+    each object is a name and id of all the chats in the collection
+    this goes onSnapshot, which mean every updata that happen on the server side will be push automaticly to the local device */
     useEffect(() => {
         let chatNames = []
         const subscriber = firestore()
@@ -37,10 +42,35 @@ function ChatScreen({ navigation, route }) {
         return subscriber;
     }, [])
 
+    /**this function is activate only by admin.
+     * on long press on some chat, an alert will pop and allow the admin to premenetly remove a chat.*/
+    function deleteChat(item) {
+        if (isAdmin) {
+            Alert.alert(
+                "Delete Chat " + item.name, "Are you sure?",
+                [{
+                    text: "DELETE",
+                    onPress: () => {
+                        firestore()
+                            .collection('chats')
+                            .doc(item.id)
+                            .delete()
+                            .then(() => console.log('delete ' + item.id + ' succssefuly'))
+                            .catch(err => console.log('error in deleting chat: ' + err.code))
+                        Alert.alert("Chats Deleted")
+                    }, style: 'destructive'
+                },
+                {
+                    text: "Cancel",
+                    style: "cancel",
+                },],
+                { cancelable: true, }
+            );
+        }
+    }
 
     return (
         <View style={styles.main}>
-
             <FlatList
                 scrollIndicatorInsets={{ right: 1 }}
                 style={styles.list}
@@ -50,18 +80,21 @@ function ChatScreen({ navigation, route }) {
                         return <View style={{ height: 40, width: '100%' }}></View>
                     else
                         return (
-                            <Chat
-                                id={item.name}
-                                onPress={() =>
-                                    navigation.navigate('GenericChat', { id: item.id, data: item })
-                                }
-                            />
+                            <Pressable onLongPress={() => deleteChat(item)}>
+                                <Chat
+                                    id={item.name}
+                                    onPress={() =>
+                                        navigation.navigate('GenericChat', { id: item.id, data: item })
+                                    }
+                                /></Pressable>
                         )
                 }}
                 keyExtractor={(item, idx) => idx}
             />
             {isAdmin ?
-                <TouchableOpacity style={styles.add} onPress={() => navigation.navigate('AddChat', { data: chats })}>
+                <TouchableOpacity
+                    style={styles.add}
+                    onPress={() => navigation.navigate('AddChat', { data: chats })}>
                     <IconIo name={'add-circle'} color={'rgb(120,90,140)'} size={65} />
                 </TouchableOpacity> : null}
         </View>
@@ -76,10 +109,8 @@ const styles = StyleSheet.create({
     list: {
         height: '100%',
         width: '100%',
-
     },
     add: {
-        borderRadius: Dimensions.get('screen').height / 3,
         position: 'relative',
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
