8a2e8a7079bcc94e95e2c0d6547252d887061820
BenSagir <bensagir.sbr@gmail.com>
2021-06-07 19:50:28 +0300

    todo and otherscreen updated

--- App/View/Screens/OtherScreen.js
+++ App/View/Screens/OtherScreen.js
@@ -2,56 +2,69 @@ import React from 'react';
 import { View, Text, StyleSheet, Touchable } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import IconIo from 'react-native-vector-icons/Ionicons';
+import IconFA5 from 'react-native-vector-icons/FontAwesome5';
+import IconFA from 'react-native-vector-icons/FontAwesome';
 
+import { Dimensions } from 'react-native';
 
 OtherScreen = ({ navigation, route }) => {
     const feed_type = route.name
     return (
-        <View style={{ alignSelf: 'center' }}>
+        <View style={styles.main}>
 
             <TouchableOpacity
                 onPress={() => navigation.navigate('ChatScreen')}
-                style={{
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    backgroundColor: '#fff000',
-                    borderRadius: 20,
-                    height: 30,
-                    width: '70%',
-
-                }}>
+                style={styles.toScreen}>
                 <IconIo
                     name={'chatbubbles'}
                     size={20}
                     color={'rgb(120,90,140)'}
                 />
-                <Text>ChatScreen</Text>
+                <Text>Chats</Text>
             </TouchableOpacity>
             <TouchableOpacity
                 onPress={() => navigation.navigate('DonationScreen')}
-                style={{
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    backgroundColor: '#fff000',
-                    borderRadius: 20,
-                    height: 30,
-                    width: '70%',
-
-                }}>
-                <IconIo
-                    name={'chatbubbles'}
+                style={styles.toScreen}>
+                <IconFA5
+                    name={'donate'}
                     size={20}
                     color={'rgb(120,90,140)'}
                 />
                 <Text>Donate</Text>
             </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => navigation.navigate('EventsScreen')}
+                style={styles.toScreen}>
+                <IconFA
+                    name={'calendar'}
+                    size={20}
+                    color={'rgb(120,90,140)'}
+                />
+                <Text>Events</Text>
+            </TouchableOpacity>
+
         </View>
 
     )
 }
 
 const styles = StyleSheet.create({
-
+    main: {
+        alignItems: 'center',
+        backgroundColor: 'rgb(200,200,220)',
+        flex: 1,
+        width: Dimensions.get('screen').width,
+        justifyContent: 'center',
+    },
+    toScreen: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        backgroundColor: '#fff000',
+        borderRadius: 20,
+        padding: 10,
+        margin: 10,
+        width: Dimensions.get('screen').width * (85 / 100),
+    },
 });
 
 
